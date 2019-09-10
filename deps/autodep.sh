ab0e67abad678b637fc062d4bd82fab1076cbadb
Bert van der Weerd <bert@stanzabird.nl>
2019-09-11 01:46:39 +0200

    updates

--- deps/autodep.sh
+++ deps/autodep.sh
@@ -9,18 +9,22 @@ if [ -z "$1" ]; then
     echo "    fedora"
     echo "    fedora-mingw64"
     echo ""
-    exit
+    exit 1
 fi
 
 case "$1" in
-    arch | archlinux) pacman -Syu --noconfirm make gcc boost
-		      ;;
-    debian) apt-get -y install make g++ libboost-all-dev
-	    ;;
-    fedora) dnf -y install make g++ boost-devel
-	    ;;
-    fedora-mingw64) dnf -y install make mingw64-gcc-c++ mingw64-boost
-		    ;;
+    arch | archlinux)
+	pacman -Syu --noconfirm make gcc boost
+	;;
+    debian)
+	apt-get -y install make g++ libboost-all-dev
+	;;
+    fedora)
+	dnf -y install make g++ boost-devel
+	;;
+    fedora-mingw64)
+	dnf -y install make mingw64-gcc-c++ mingw64-boost
+	;;
     *) echo "autodep.sh: error: arument not a valid DISTRO specifier"
+       exit 1
 esac
-
